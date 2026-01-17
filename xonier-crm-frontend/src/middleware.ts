79fe840a975abcaa669824d23e8ab0d804392abf
xonier-mridul <mridulsaklanixonier@gmail.com>
2026-01-17 12:02:22 +0530

    ad

--- xonier-crm-frontend/src/middleware.ts
+++ xonier-crm-frontend/src/middleware.ts
@@ -1,48 +1,53 @@
-import {NextRequest, NextResponse} from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
-const PROTECTED_ROUTES: Array<string> = ["/dashboard", "/users", "/roles", "/enquiry", "/teams"]
+const PROTECTED_ROUTES = ["/dashboard", "/users", "/roles", "/enquiry", "/teams"];
 const API_BASE_URL = process.env.API_BASE_URL!;
-export async function middleware(request: NextRequest){
-    const {pathname} = request.nextUrl;
 
-    const accessToken = request.cookies.get("accessToken")?.value;
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  let isAuthenticated = false;
 
-    let isAuthenticated: boolean = false;
-
-
-    try {
-        const res = await fetch(`${API_BASE_URL}/auth/me`, {
-        method:"GET",
-        headers: {
+  try {
+    const res = await fetch(`${API_BASE_URL}/auth/me`, {
+      headers: {
         cookie: request.headers.get("cookie") || "",
-      }
-    })
+      },
+    });
 
-    if(res.status === 200){
-      const json = await res.json()
-      isAuthenticated = true
-      console.log("chuk dum dum")
+    if (res.status === 200) {
+      isAuthenticated = true;
     }
-    } catch (error) {
-        isAuthenticated = false
-        console.log("naiyo re")
-    }
+  } catch (err) {
+    isAuthenticated = false;
+  }
 
-    if(PROTECTED_ROUTES.some((item)=> pathname.startsWith(item) && !isAuthenticated)){
-        const loginUrl = new URL("/login", request.url);
+  if (
+    PROTECTED_ROUTES.some(
+      (route) => pathname.startsWith(route) && !isAuthenticated
+    )
+  ) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
-        return NextResponse.redirect(loginUrl);
-    }
+  if (pathname.startsWith("/login") && isAuthenticated) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
 
-    if(pathname.startsWith("/login") && isAuthenticated){
-        const dashboardUrl = new URL("/dashboard", request.url);
-        return NextResponse.redirect(dashboardUrl);
-    }
-
-    return NextResponse.next()
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/dashboard", "/dashboard/:path*", "/login", "/users", "/users/:path*", "/roles", "/roles/:path*", "/enquiry", "/enquiry:path*", "/teams", "/teams/:path*"]
-}
-
+  matcher: [
+    "/dashboard",
+    "/dashboard/:path*",
+    "/login",
+    "/users",
+    "/users/:path*",
+    "/roles",
+    "/roles/:path*",
+    "/enquiry",
+    "/enquiry/:path*",
+    "/teams",
+    "/teams/:path*",
+  ],
+};
