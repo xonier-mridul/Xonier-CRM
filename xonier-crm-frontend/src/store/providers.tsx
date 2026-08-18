022a99c6b5e66f62b4341b42c786f5447305a342
Rahul Gushain <rahul@xonier.in>
2026-08-18 14:38:13 +0530

    lead commit

--- xonier-crm-frontend/src/store/providers.tsx
+++ xonier-crm-frontend/src/store/providers.tsx
@@ -11,15 +11,16 @@ export default function ReduxProvider({ children }: {
   const pathname = usePathname();
 
   const publicPaths = [
+
     "/login",          // Standard login page
     "/admin-login",    // Alternative admin login page (if applicable)
     "/forgot-password", // Forgot password page
     "/forgot-companyId",
     "/change-password"
+
   ];
 
-  // 2. Check if the current URL matches any of the public paths
-  // (We also check startsWith to handle sub-paths or dynamic route segments)
+
   const isPublicRoute = publicPaths.some(
     (path) => pathname === path || pathname?.startsWith(path + "/")
   );
